5aa24259f024ef53b652581e9b6da7cb7e899229
Andrey Semenov <andss82@gmail.com>
2021-02-27 23:29:17 +0300

    initial

---
+++ server.js
@@ -0,0 +1,60 @@
+const http = require('http')
+const Koa = require('koa')
+const Router = require('koa-router')
+const cors = require('koa2-cors')
+const koaBody = require('koa-body')
+
+const app = new Koa()
+
+app.use(
+  cors({
+    origin: '*',
+    credentials: true,
+  })
+)
+
+app.use(koaBody({ json: true }))
+
+const notes = [
+  {
+    id: 0,
+    content: 'test',
+  },
+  {
+    id: 1,
+    content: 'test test',
+  },
+  {
+    id: 2,
+    content: 'test test test',
+  },
+]
+let nextId = 3
+
+const router = new Router()
+
+router.get('/notes', async (ctx, next) => {
+  ctx.response.body = notes
+})
+
+router.post('/notes', async (ctx, next) => {
+  notes.push({ ...ctx.request.body, id: nextId++ })
+  console.log(notes)
+  ctx.response.status = 204
+})
+
+router.delete('/notes/:id', async (ctx, next) => {
+  console.log(ctx.params.id)
+  const noteId = Number(ctx.params.id)
+  const index = notes.findIndex((o) => o.id === noteId)
+  if (index !== -1) {
+    notes.splice(index, 1)
+  }
+  ctx.response.status = 204
+})
+
+app.use(router.routes()).use(router.allowedMethods())
+
+const port = process.env.PORT || 7777
+const server = http.createServer(app.callback())
+server.listen(port, () => console.log('server started'))
